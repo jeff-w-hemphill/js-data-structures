8b757b2be22abe2da00ccab198bc0b0a29ac54c4
Jeffery Hemphill <jefferywmeyers@gmail.com>
2022-08-24 23:17:13 -0600

    Added examples

---
+++ ForOfIn.js
@@ -0,0 +1,19 @@
+let x = [4, 5, 3];
+let s = 'abc def';
+
+for (let i in x) {
+    console.log(i);
+}
+
+for (let val of x) {
+    console.log(val);
+}
+
+for (let i in s) {
+    console.log(i)
+}
+
+for (let val of s) {
+    console.log(val)
+}
+
